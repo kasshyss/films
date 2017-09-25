5cece4bed1d9444760f863446d98000934d43c2f
Meriadoc <pierre@bourgain.net>
2017-09-25 17:07:15 +0200

    add display note in web app

--- appli/films.py
+++ appli/films.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 language = '_fr'
 
 @app.route('/')
+@app.route('/index/')
+@app.route('/index')
 def index():
     label_conf = conf.get_conf('template' + language + '.conf')
     return render_template(
@@ -21,18 +23,31 @@ def index():
 def creator():
     label_conf = conf.get_conf('template' + language + '.conf')
     return render_template(
-            'creator.html'
+            'display.html'
             ,app_name = label_conf['app_name']
+            ,button_label = label_conf['return_button']
             ,title = label_conf['creator_title']
-            ,directors = io.get_directors_nat()
+            ,display_list = io.get_directors_nat()
             )
 
 @app.route('/films/')
 def film():
     label_conf = conf.get_conf('template' + language + '.conf')
     return render_template(
-            'films.html'
+            'display.html'
             ,app_name = label_conf['app_name']
+            ,button_label = label_conf['return_button']
             ,title = label_conf['film_title']
-            ,films = io.get_films()
+            ,display_list = io.get_films()
+            )
+
+@app.route('/notes/')
+def notes():
+    label_conf = conf.get_conf('template' + language + '.conf')
+    return render_template(
+            'display.html'
+            ,app_name = label_conf['app_name']
+            ,button_label = label_conf['return_button']
+            ,title = label_conf['note_title']
+            ,display_list = io.get_notes_films()
             )
